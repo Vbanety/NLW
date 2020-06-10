cbcc4ae8bb165462e954b9b596c0c885fef1a686
Vinicius Batista <57818111+Vbanety@users.noreply.github.com>
2020-06-09 21:11:34 -0300

    Revisar alterações
    Verifica a necessidade da pasta modules

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NLW
 Next Level Week
+
+Subindo os arquivos criados durante aula para o github.
---
+++ public/scripts/anotacoes.js
@@ -0,0 +1,67 @@
+//coments
+// document.write("Hello world")
+
+//variaveis, tipos de dados
+
+/*
+let myvar
+myvar = "Hello word"
+
+const myconst = "he"
+
+document.write(myconst + myvar)
+
+const s1 = "isso é uma string"
+const s2 = 'isso também é um string'
+const s3 = `isso tbm é um string`
+
+//number
+
+const n1 = 1
+const n2 = 12
+document.write(n1 + n2)
+
+//boolean = true or false
+
+const bTrue = true
+const bFalse = false
+
+document.write(bFalse)
+
+
+//objeto possuem propriedades e funcionalidades
+
+const pessoa = {
+  altura: "1,80m",
+  idade: 24,
+  solteiro: true,
+  correr(){
+    document.write("Executar uma grande logica de correr")
+  }
+}
+pessoa.correr()
+
+//Arrey - Vetores
+const  colecaoDeBolinhas = ["blue", "green", 1, {name:"My name"}]
+
+document.write(colecaoDeBolinhas[3].name)
+*/
+/*
+function sayMyName(name) {
+  document.write(name)
+}
+
+sayMyName("Vinicius, ")
+sayMyName("Juliano e ")
+sayMyName("Jonas")
+
+
+// loop repetiçoes
+
+for (i = 0; i <= 10; i++) {
+    document.write(`<p>${i}</p>`)
+  }
+  
+  
+  */
+  
